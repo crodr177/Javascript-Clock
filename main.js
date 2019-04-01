dc0bafd3e052253e3cfc6c29f7205641a096c68d
Cruz Rodriguez <cruzrodriguez1127@gmail.com>
2019-04-01 16:29:46 -0700

    hard mode

--- main.js
+++ main.js
@@ -1,5 +1,45 @@
-var time = document.querySelector("#time");
+var colorText = document.querySelector("#color");
+var colorChange = document.querySelector("#color-change");
+
+var interval = 5;
+var red = 0;
+var blue = 0;
+var green = 0;
+
+function pad(str) {
+  if (str.length === 1) {
+    return "0" + str;
+  } else {
+    return str;
+  }
+}
 
 setInterval(function(){
-  time.innerHTML = moment().format("hh:mm:ss a");
-});
+  blue += interval;
+
+  if (blue >= 255) {
+    blue = 0;
+    green += interval;
+  }
+
+  if (green >= 255) {
+    green = 0;
+    red += interval;
+  }
+
+  if (red >= 255) {
+    red = 0;
+    blue = 0;
+    green = 0;
+  }
+
+  let redstr = pad(red.toString(16));
+  let greenstr = pad(green.toString(16));
+  let bluestr = pad(blue.toString(16));
+
+
+  var hexaColor = redstr + greenstr + bluestr;
+
+  colorText.innerHTML = `#${hexaColor}`;
+  colorChange.style.backgroundColor = `#${hexaColor}`;
+}, 1000);
